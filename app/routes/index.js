2241edd8732aef0446ab3870800ab7f9b739b893
Levi Gross <levi@levigross.com>
2016-07-17 10:11:45 -0400

    That should be the last of the comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -49,17 +49,12 @@ var exports = function(app, db) {
     // Benefits Page
     app.get("/benefits", isLoggedIn, benefitsHandler.displayBenefits);
     app.post("/benefits", isLoggedIn, benefitsHandler.updateBenefits);
-    /* Fix for A7 - checks user role to implement  Function Level Access Control
-     app.get("/benefits", isLoggedIn, isAdmin, benefitsHandler.displayBenefits);
-     app.post("/benefits", isLoggedIn, isAdmin, benefitsHandler.updateBenefits);
-     */
 
     // Allocations Page
     app.get("/allocations/:userId", isLoggedIn, allocationsHandler.displayAllocations);
 
     // Handle redirect for learning resources link
     app.get("/learn", isLoggedIn, function(req, res, next) {
-        // Insecure way to handle redirects by taking redirect url from query string
         return res.redirect(req.query.url);
     });
 
